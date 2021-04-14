f40dc5fbd71f6ee072e7afce8896bb30159ced31
Rafaela Georgescu <rafaela.georgescu@accesa.eu>
2021-04-14 23:20:36 +0300

    added faq page and url settings

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'pages/index.html')
+
 
 def about(request):
     return render(request, 'pages/about.html')
 
+
 def contact(request):
     return render(request, 'pages/contact.html')
+
+
+def faq(request):
+    return render(request, 'pages/faq.html')
 
