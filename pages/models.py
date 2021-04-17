134ad2a7a91d4c8d5980015fa863bbe5e59f17e7
Rafaela Georgescu <rafaela.georgescu@accesa.eu>
2021-04-17 13:48:02 +0300

    added aboutpage model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import ImageField
+from ckeditor.fields import RichTextField
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+def validate_only_one_instance(obj):
+    model = obj.__class__
+    if (model.objects.count() > 0 and obj.id != model.objects.get().id):
+        raise ValidationError("Can only create 1 %s instance" % model.__name__)
+
+
+class AboutPage(models.Model):
+    photo_1 = ImageField(upload_to='photos/about/')
+    photo_2 = ImageField(upload_to='photos/about/')
+    content = RichTextField()
+
+    def clean(self):
+        validate_only_one_instance(self)
+
+    def __str__(self):
+        return 'About page'
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from pages.models import AboutPage
 
 
 def index(request):
     return render(request, 'pages/index.html')
 
 
-def about(request):
-    return render(request, 'pages/about.html')
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of first aboutPage template
+        context['about_page'] = AboutPage.objects.first()
+        return context
 
 
 def contact(request):
