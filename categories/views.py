0a39dd3ba7ddb361abd60f1fb520d17db4886c2a
szilardvig <39991646+szilardvig@users.noreply.github.com>
2021-04-25 02:44:05 +0300

    added category page and made necessary changes

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Category, Gallery
+
+def category(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    gallery = Gallery.objects.filter(category_id=category_id)
+
+    context = {
+        'category': category,
+        'gallery': gallery
+    }
+
+    return render(request, 'pages/category.html', context)
