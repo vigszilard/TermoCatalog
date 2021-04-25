b2b37a897bed83fc867eaef692fcf9bd28d3d52e
Rafaela Georgescu <rafaela.georgescu@accesa.eu>
2021-04-25 03:32:22 +0300

    added contact page and model; updated font

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, AboutPageView, contact, faq
+from .views import index, AboutPageView, ContactPageView, faq
 
 urlpatterns = [
     path('', index, name='index'),
     path('about', AboutPageView.as_view(), name='about'),
-    path('contact', contact, name='contact'),
+    path('contact', ContactPageView.as_view(), name='contact'),
     path('faq', faq, name='faq')
 ]
