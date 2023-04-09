5632eb53ee1869e03d323db299070070d23a2826
szilardvig <39991646+szilardvig@users.noreply.github.com>
2023-04-09 23:22:05 +0300

    added product number

--- products/admin.py
+++ products/admin.py
@@ -4,9 +4,10 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'display_img_preview')
-    list_display_links = ('id', 'name')
+    list_display = ('id', 'name', 'number', 'category', 'display_img_preview')
+    list_display_links = ('id', )
     list_filter = ('category',)
-    sortable_by = ('id', 'name', 'category')
+    sortable_by = ('id', 'name', 'number', 'category')
     search_fields = ('name',) 
     readonly_fields = ('display_img_preview',)
+    list_editable = ('name', 'number', )
--- products/models.py
+++ products/models.py
@@ -9,13 +9,14 @@ def product_dir_path(instance, filename):
 
 class ProductManager(models.Manager):
     def get_queryset(self):
-        return super(ProductManager, self).get_queryset().order_by('category', 'name')
+        return super(ProductManager, self).get_queryset().order_by('category', 'name', 'number')
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    number = models.PositiveSmallIntegerField(blank=True, default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     display_img = models.CharField(max_length=250)
-    description = RichTextField()
+    description = RichTextField(blank=True)
     youtube_link = models.CharField(max_length=250, blank=True)
     external_link = models.CharField(max_length=250, blank=True)
 
