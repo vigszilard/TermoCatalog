038470eb1e2dca5046902a91c926d481a0026e45
szilardvig <39991646+szilardvig@users.noreply.github.com>
2023-04-09 23:53:51 +0300

    modified admin view of products

--- products/admin.py
+++ products/admin.py
@@ -4,10 +4,10 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'number', 'category', 'display_img_preview')
+    list_display = ('id', 'name', 'number', 'category', 'description', 'display_img_preview')
     list_display_links = ('id', )
     list_filter = ('category',)
     sortable_by = ('id', 'name', 'number', 'category')
     search_fields = ('name',) 
     readonly_fields = ('display_img_preview',)
-    list_editable = ('name', 'number', )
+    list_editable = ('name', 'number', 'description')
