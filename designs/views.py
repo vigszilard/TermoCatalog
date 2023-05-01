6ecaf4d65d87a289b11c2e3318ebbf4bdceb56f6
szilardvig <39991646+szilardvig@users.noreply.github.com>
2023-05-01 20:40:05 +0300

    Added paginator

--- designs/views.py
+++ designs/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Sandblast, Printing
 
 
 def sandblast(request):
     sandblasts = Sandblast.sorted_by_size.all()
+    paginator = Paginator(sandblasts, 12)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'sandblasts': sandblasts
+        'sandblasts': sandblasts,
+        'page_obj': page_obj
     }
 
     return render(request, 'pages/sandblast.html', context)
@@ -15,9 +21,14 @@ def sandblast(request):
 
 def printing(request):
     printings = Printing.sorted_by_size.all()
+    paginator = Paginator(printings, 12)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'printings': printings
+        'printings': printings,
+        'page_obj': page_obj
     }
 
     return render(request, 'pages/printing.html', context)
